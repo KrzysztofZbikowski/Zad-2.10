031098c1a9710f788d6775478b5c058e1be7e2ed
Zbiku <Zbiku@MSI>
2023-10-19 22:54:33 +0200

    Dodaj pliki projektów.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,10 @@
+﻿class Zad
+{
+    static void Main(string[] args)
+    {
+        int powierzchnia = 100, osoby = 10;
+        double gestoscZaludnienia = Convert.ToDouble(osoby)/ powierzchnia;
+        Console.WriteLine(gestoscZaludnienia);
+        Console.ReadKey();
+    }
+}
